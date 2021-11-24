4921dd18d0a81b89914689a8518f1442b16532dc
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2021-11-24 16:21:11 +0800

    Day10: Calculator

---
+++ Day10_Functions with outputs/Day10.py
@@ -0,0 +1,71 @@
+# Calculator
+
+# add function
+def add(n1, n2):
+    return n1 + n2
+
+
+# subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+
+# Multiply
+def Multiply(n1, n2):
+    return n1 * n2
+
+
+# Divide
+def Divide(n1, n2):
+    return n1 / n2
+
+
+# Create a dict{symbol:function}
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": Multiply,
+    "/": Divide
+}
+# above value are functions
+
+
+# ask user to input
+num1 = int(input("What's the first number?: "))
+
+# loop through the dict and print out the symbol
+for symbol in operations:
+    print(symbol)
+
+# ask symbol
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+
+# operation_function is function
+operation_function = operations[operation_symbol]
+# call the function
+answer = operation_function(num1, num2)
+
+# display
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+# continue_flag
+continue_flag = True
+
+while continue_flag:
+    # ask user to continue or not
+    con_calculation = input(f"Type 'y' to continue calculation with {answer}, or type 'n' to exit.:")
+
+    if con_calculation == "n":
+        continue_flag = False
+        if continue_flag == False:
+            break
+    # ask for another num and operation
+    operation2 = input("Pick anther operation: ")
+    num3 = int(input("What's the next number?: "))
+    operation_function = operations[operation2]
+    past_answer = answer
+    answer = operation_function(answer, num3)
+
+    # display
+    print(f"{past_answer} {operation2} {num3} = {answer}")
