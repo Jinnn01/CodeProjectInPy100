0da3173f83ef22c7c5d6466470af9abc037cd6e7
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2021-11-29 21:31:33 +0800

    Day14: fix the bug which can not break

--- Day14_HigherLowerGame/Day14_start.py
+++ Day14_HigherLowerGame/Day14_start.py
@@ -3,9 +3,8 @@ import art
 import game_data
 import random
 
-
-def play_game(final_score):
-
+print(art.logo)
+def play_game():
     # get data from game_data randomly and make sure
 
     Compare_A = random.choice(game_data.data)
@@ -15,9 +14,8 @@ def play_game(final_score):
 
     print_Astring = f"Compare A:{Compare_A['name']}, {Compare_A['description']}, from{Compare_A['country']}"
     # hint for test
-    print(Compare_A['follower_count'],Against_B['follower_count'])
+    print(Compare_A['follower_count'], Against_B['follower_count'])
     print_Bstring = f"Against B:{Against_B['name']}, {Against_B['description']}, from{Against_B['country']}"
-    # print(art.logo)
     print(print_Astring)
     print(art.vs)
     print(print_Bstring)
@@ -26,24 +24,28 @@ def play_game(final_score):
         result = 1  # means A>B
     else:
         result = -1  # means A<B
+    return result
+
+final_score = 0
+while True:
+    result = play_game()
+    # ask uer to input
+    user_choice = input("Who has more followers? Type 'A' or 'B':")
+    if user_choice == 'A':
+        user_choice = 1
+    elif user_choice == 'B':
+        user_choice = -1
 
 
-    while True:
-        # ask uer to input
-        user_choice = input("Who has more followers? Type 'A' or 'B':")
-        if user_choice == 'A':
-            user_choice = 1
-        elif user_choice == 'B':
-            user_choice = -1
+    if result == user_choice:
+        final_score += 1
+        print(f"You're right!Current score: {final_score}.")
+        # keep ask asking
+        # score +1
+    else:
+        print(f"Sorry, that's wrong. Final score: {final_score}")
+        break
+
+    print(art.logo)
 
 
-        if result == user_choice:
-            final_score += 1
-            print(f"You're right!Current score: {final_score}.")
-            play_game(final_score)
-            # keep ask asking
-            # score +1
-        else:
-            print(f"Sorry, that's wrong. Final score: {final_score}")
-            return final_score
-play_game(0)
